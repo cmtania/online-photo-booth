f94c1d3e1f377268e08913d934b9caacfd4f7df1
tania.dev <taniachristian01@gmail.com>
2025-06-29 00:25:43 +0800

    Refactor Camera Layout and add background

--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { CameraComponent } from '../components/camera/camera.component';
+import { SelectModel } from '../models/count-select.model';
 
 @Component({
   selector: 'app-layout',
@@ -18,6 +19,28 @@ export class LayoutComponent {
   photos: string[] = [];
   countdownTime: number = 3; // Default countdown time
 
+  countSelection: SelectModel[] =  [
+    { Label: '1 Photo', Value: 1 },
+    { Label: '2 Photos', Value: 2 },
+    { Label: '3 Photos', Value: 3 },
+    { Label: '4 Photos', Value: 3 }
+  ];
+
+  timerSelection: SelectModel[] =  [
+    { Label: '1 Second', Value: 1 },
+    { Label: '3 Seconds', Value: 3 },
+    { Label: '5 Seconds', Value: 5 },
+    { Label: '10 Seconds', Value: 10 }
+  ];
+
+  backgroundSelection: SelectModel[] =  [
+    { Label: 'Everlasting Sky', Value: "background-image: linear-gradient(135deg, #fdfcfb 0%, #e2d1c3 100%);" },
+    { Label: 'Jungle Day', Value:  "background-image: linear-gradient(45deg, #8baaaa 0%, #ae8b9c 100%);" },
+    { Label: 'Warm Flame', Value: "background-image: linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);"},
+    { Label: 'Tempting Azure', Value: "background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);" }
+  ];
+  
+
   onPhotoCaptured(photo: string): void {
     this.photos.push(photo);
   }
@@ -28,5 +51,10 @@ export class LayoutComponent {
 
   onCameraSelected(cameraId: string): void {
     this.cameraComponent.onCameraSelected(cameraId);
+  }
+
+  onTimerSelected($event: any): void {
+    const value = ($event.target as HTMLSelectElement).value;
+    this.countdownTime = Number(value);
   }
 }
---
+++ src/app/modules/models/count-select.model.ts
@@ -0,0 +1,4 @@
+export class SelectModel {
+  Value: any;
+  Label: string = "";
+}
